6b6442b5bdc3a3b4b646cd80e46fa062dcd732f0
ericparadis <33237433+ericparadis@users.noreply.github.com>
2019-07-18 09:53:54 -0500

    mood updates

--- server/routes/api/movies.js
+++ server/routes/api/movies.js
@@ -8,12 +8,14 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     const movies = await loadMovieCollection();
 
-    const by = req.query.by || 'year'
-
+    const by = req.query.by || 'year';
+    const title = req.query.title;
     const count = parseInt(req.query.count) || 10;
     const page = parseInt(req.query.page) || 1;
     const year = req.query.year || 'all';
-    const imdbId = req.query.imdbId || null;
+    const mood = req.query.mood;
+    const moodValue = req.query.moodValue || 50;
+    const imdbId = req.query.imdbId;
     const sortValue = req.query.sort || 'Title';
     const order = req.query.order || 1;
 
@@ -39,7 +41,57 @@ router.get('/', async (req, res) => {
         res.send(await movies.aggregate([{ $sample: { size: 1 } }]).toArray());
     }
     if (by === 'imdbId') {
-        res.send(await movies.find({imdbID: imdbId}).toArray());
+        res.send(await movies.find({"imdbID": imdbId }).toArray());
+    }
+    // if (by === 'mood') {
+    //     res.send(await movies
+    //         .find({ [mood] : { $exists: true } })
+    //         .sort( { [mood] : -1 } )
+    //         .collation({locale: "en_US", numericOrdering: true})
+    //         .skip(count*(page-1))
+    //         .limit(count)
+    //         .toArray());
+    // }
+    if (by === 'mood') {
+        res.send(await movies
+            .aggregate([
+                {$match: {"Anger" : { $exists: true }}},
+                {$project: {
+                    AngerDiff: {$abs: {$subtract: [{$toDecimal: moodValue}, {$toDecimal: "$Anger"}]}},
+                    AnticipationDiff: {$abs: {$subtract: [{$toDecimal: moodValue}, {$toDecimal: "$Anger"}]}}, 
+                    JoyDiff: {$abs: {$subtract: [{$toDecimal: moodValue}, {$toDecimal: "$Anger"}]}}, 
+                    TrustDiff: {$abs: {$subtract: [{$toDecimal: moodValue}, {$toDecimal: "$Anger"}]}}, 
+                    FearDiff: {$abs: {$subtract: [{$toDecimal: moodValue}, {$toDecimal: "$Anger"}]}}, 
+                    SurpriseDiff: {$abs: {$subtract: [{$toDecimal: moodValue}, {$toDecimal: "$Anger"}]}}, 
+                    SadnessDiff: {$abs: {$subtract: [{$toDecimal: moodValue}, {$toDecimal: "$Anger"}]}}, 
+                    DusgustDiff: {$abs: {$subtract: [{$toDecimal: moodValue}, {$toDecimal: "$Anger"}]}}, 
+                    doc: '$$ROOT'}},
+                {$sort: {diff: 1}}
+            ])
+            .skip(count*(page-1))
+            .limit(count)
+            .toArray());
+    }
+    if (by === 'title') {
+        res.send(await movies
+            .find({"Title": {$regex : title} })
+            .sort({"imdbRating": -1})
+            .skip(count*(page-1))
+            .limit(count)
+            .toArray());
+    }
+    if (by === 'prime') {
+        const windows = await loadWindowsCollection()
+        const prime = await windows
+            .find({ "name" : "prime" })
+            .toArray();
+
+        res.send(await movies
+            .find({"imdbID": { $in: prime[0].ids } })
+            .sort({"imdbRating": -1})
+            .skip(count*(page-1))
+            .limit(count)
+            .toArray());
     }
 });
 
@@ -111,6 +163,14 @@ async function loadMovieCollection() {
         useNewUrlParser: true
     });
     return client.db('mood2watch').collection('movies'); 
+}
+
+async function loadWindowsCollection() {
+    const client = await mongodb.MongoClient
+    .connect('mongodb+srv://mood:mood@cluster0-nei35.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+    return client.db('mood2watch').collection('windows'); 
 }
 
 
---
+++ server/routes/api/windows.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get Windows
+router.get('/', async (req, res) => {
+    const windows = await loadWindowCollection();
+    const count = parseInt(req.query.count) || 10;
+    const page = parseInt(req.query.page) || 1;
+    res.send(await windows.find({}).sort( { rank: 1 } ).skip(count*(page-1)).limit(count).toArray());
+});
+
+//Get Window by channel
+router.get('/:channel', async (req, res) => {
+    const windows = await loadWindowCollection();
+    res.send(await windows.findOne({channel: req.params.channel}));
+});
+
+//Add Window
+router.post('/', async (req, res) => {
+    const windows = await loadWindowCollection();
+    await windows.insertOne({
+        name: req.body.name,
+        ids: req.body.ids,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+//Update Window by ID
+router.put('/:id', async (req, res) => {
+    const windows = await loadWindowCollection();
+    await windows.updateOne({_id: new mongodb.ObjectID(req.params.id)},{$set: req.body});
+    res.status(200).send();
+});
+
+//Delete Window
+router.delete('/:id', async (req, res) => {
+    const windows = await loadWindowCollection();
+    await windows.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadWindowCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://mood:mood@cluster0-nei35.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+    return client.db('mood2watch').collection('windows');
+}
+
+module.exports = router;
